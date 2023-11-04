1f6df2715501faf03304c268a893300ad6b57c53
TMPadma <149878519+TMPadma@users.noreply.github.com>
2023-11-04 17:52:30 +0530

    Add files via upload

---
+++ My_First_Code.R
@@ -0,0 +1,64 @@
+# You are an awesome person!
+3+4
+getwd()
+H <- c(7,12,28,3,41)
+M <- c('Mar', 'Apr', 'May', 'Jun', 'Jul')
+# Plot the bar chart 
+barplot(H,names.arg=M,xlab='Month',ylab='Revenue',col ='blue', main='Revenue chart',border='red')
+H <- c(7,12,28,3,41)
+M <- c('Mar', 'Apr', 'May', 'Jun', 'Jul')
+# Plot the bar chart 
+barplot(H,names.arg=M,xlab='Month',ylab='Revenue',col ='blue', main='Revenue chart',border='red')
+v <- c(9,13,21,8,36,22,12,41,31,33,19) 
+# Create the histogram. 
+hist(v,xlab = "Weight",col = "yellow",border =  "blue")
+df=read.csv('COVID19+Data+Viz+Postcode+data+-+postcode (1)')
+getwd()
+getwd()
+df=read.csv('ebola.csv')
+df
+view(df)
+tibble::view(df)
+tibble::view(df)
+head(df)
+tail(df)
+df[,2]
+df[,2][1:5]
+df[,5][1:8]
+df_subset=df[(df$Country=='Uganda'),]
+df_subset
+df_subset=df[(df$Country=='Uganda'),]
+df_subset
+dim(df)
+str(df)
+dim(df)
+str(df)
+length(df$Country)
+mean(df$Year)
+median(df$Year)
+write.table(df_subset,'new_df.csv',sep=',')
+write.table(df_subset,'new_df.csv',sep=',')
+getwd()
+in
+our.mean<- function(x){
+  sum(x)/length(x)
+}
+our.mean(1:5)
+our.mean2<-function(x){
+  n=length(x)
+  s=sum(x)
+  output=s/n
+  o
+our.mean2(1:5)
+GM <- function(x){
+  prod(x) ^ (1/length(x))
+}
+GM(5)
+GM<- function(x){
+  prod(x)^(1/lenght(x))
+
+}
+a=c(0,0,0,0,0,0)
+a
+a[2]=1
+a
