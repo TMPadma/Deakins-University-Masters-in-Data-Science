589bd1a0d6b4dc5985adf74ac95aca2f19122263
TMPadma <149878519+TMPadma@users.noreply.github.com>
2023-11-04 20:36:51 +0530

    Add files via upload

---
+++ mtcars.R
@@ -0,0 +1,34 @@
+getwd()
+getwd()
+a<- 5
+print(a)
+a
+class(a)
+x <- 100L
+print(x)
+class(x)
+# comment is the ash symbol
+# it is a case sensitive
+x<- 9i+5
+x
+class(x)
+x
+x<-9i+5
+x
+class(x)
+help.start()
+?typeof()
+data()
+mtcars
+View('mtcars')
+head(mtcars)
+tail(mtcars)
+mtcars_mpg<-mtcars$mpg[(mtcars$mpg<=20)]
+mtcars_mpg
+hist(mtcars$cyl)
+plot(mtcars)
+plot(mtcars[,2],mtcars[,3])
+a<-5
+b<-2
+c<-prod(a,b)
+c
